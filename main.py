3d040e55cf5860e275f2096caf39d3bd23afe808
Twice_Ice <125768114+Twice-Ice@users.noreply.github.com>
2024-03-05 10:49:04 -0700

    got particles working
    only bug is that they don't go to the left side of the particle emitter at all...

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ doExit = False
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_SIZE))
 
-testParticleEmitter = ParticleEmitter(Vector2(SCREEN_SIZE)//2, 2, 1)
+testParticleEmitter = ParticleEmitter(Vector2(SCREEN_SIZE)//2, 100, 1)
 
 boss = Boss((400, 400))
 guy = Player(1)
--- particles.py
+++ particles.py
@@ -5,32 +5,30 @@ from pygame import Vector2
 from globals import SCREEN_RECT
 
 class Particle:
-	def __init__(self, pos = Vector2(0, 0), color = (255, 255, 255), time = 10, size = 5): #these positions will be relative to the position of the particle emitter.
+	def __init__(self, pos = Vector2(0, 0), color = (255, 255, 255), time = 1000, size = 5): #these positions will be relative to the position of the particle emitter.
 		self.pos = pos
 		self.velo = Vector2(0, 0)
 		self.color = color
 		self.time = time
 		self.size = size
 
-	def update(self, screen, pos):
-		self.draw(screen, pos)
-		# self.gravity()
+	def update(self, screen, emitterPos):
+		self.draw(screen, emitterPos)
+		self.gravity()
 		self.randMove()
 		self.time -= 1
 	
-	def draw(self, screen, pos):
-		print("drew")
-		pygame.draw.circle(screen, self.color, self.pos + pos, self.size)
+	def draw(self, screen, emitterPos):
+		pygame.draw.circle(screen, self.color, self.pos + emitterPos, self.size)
 
 	def gravity(self):
-		if self.velo.y <= 1:
-			self.velo.y += .05
+		if self.velo.y <= 100:
+			self.velo.y += .25
 		self.pos.y += self.velo.y
 
 	def randMove(self):
-		self.velo = Vector2(random.randint(-5, 5)/10, random.randint(-5, 5)/10)
+		self.velo += Vector2(random.randint(-5, 5)/10, 0)
 		self.pos += self.velo
-
 
 class ParticleEmitter:
 	def __init__(self, pos = Vector2(0, 0), maxParticles = 10, ppf = 1): #ppf = particles per frame
@@ -40,13 +38,12 @@ class ParticleEmitter:
 		self.ppf = ppf
 
 	def update(self, screen, pos = None):
-		print()
 		if pos != None:
 			self.pos = pos
 
 		for i in range(self.ppf):
 			if len(self.particleList) <= self.maxParticles:
-				self.particleList.append(Particle())
+				self.particleList.append(Particle(Vector2(0, 0)))
 
 		for i in range(len(self.particleList)-1, 0, -1):
 			self.particleList[i].update(screen, self.pos)
